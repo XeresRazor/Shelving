face4731a7f2a7491524fc3b33cbabba2ce36a87
Erin <casio913@Ambrosius.local>
2011-10-24 20:36:21 -0400

    Initial Commit

---
+++ Shelving/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Shelving
+//
+//  Created by Erin Green on 10/24/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Shelving/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Shelving
+//
+//  Created by Erin Green on 10/24/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
